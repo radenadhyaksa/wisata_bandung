2ae6dd403cc95ef0ee8685b1b35464ad5413d137
radenadhyaksa <raksa69@gmail.com>
2022-03-23 16:01:31 +0700

    update file tourism_place

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_bandung/model/tourism_place.dart';
 
+
+// AppBaruntuk judul halaman
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
-
-// AppBaruntuk judul halaman
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wisata Bandung',
-      theme: ThemeData(),
-      home: MainScreen(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wisata Bandung'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final TourismPlace place = tourismPlaceList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen();
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(place.imageAsset),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            place.name,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(place.location),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: tourismPlaceList.length,
+      ),
     );
   }
 }
